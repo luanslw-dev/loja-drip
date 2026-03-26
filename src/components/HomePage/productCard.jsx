1ad3e75b7506e2d8e733e035189ddcb2924a63c9
luanslw-dev <163569141+luanslw-dev@users.noreply.github.com>
2026-03-26 12:10:48 -0300

    atualizaçoes3

--- src/components/HomePage/productCard.jsx
+++ src/components/HomePage/productCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useCart } from "../../contexts/CartContext";
+import { useCart } from "../../contexts/cartContext";
 
 
 const ProductCard = ({ product = {} }) => {
